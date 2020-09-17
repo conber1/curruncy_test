196ae92a11eac2a0708311a8ae3386eba92c03fc
conber1 <48555903+conber1@users.noreply.github.com>
2020-09-17 15:24:29 +0300

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from handlers.converter import *
 from handlers.db import *
 
+
 def setup_routes(app):
     app.router.add_post("/database", insert_record), #adding urls...
     app.router.add_get("/convert", convert_data)
